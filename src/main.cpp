33a803ef4902977fe4bc584b8ffaa95fd29953b3
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2023-10-11 22:46:25 -0300

    fondo con color

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
+void processInput(GLFWwindow* window);
 
 int main() {
+	// Init GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// Create window object
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
 		std::cerr << "Failed to create GLFW window" << std::endl;
@@ -19,16 +22,25 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 
+	// Check glad
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}  
 
+	// Manage viewport resize
 	glViewport(0, 0, 800, 600);
-
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// Render loop
 	while(!glfwWindowShouldClose(window)) {
+		// Input
+		processInput(window);
+
+		// Render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Check calls events and swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();    
 	}
@@ -40,4 +52,10 @@ int main() {
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 } 
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 

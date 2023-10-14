16a306fe91c49cee66b9bec497b027a889c623f4
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2023-10-14 11:07:40 -0300

    2 triangulos 1 VBO

--- src/main.cpp
+++ src/main.cpp
@@ -89,15 +89,22 @@ int main() {
 
 	// Triangle vertex data
 	float vertices[] {
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+		// Left triangle
+		-0.1f, 0.5f, 0.0f,
+		-0.1f, -0.5f, 0.0f,
+		-0.5f, 0.0f, 0.0f,
+
+		// Left triangle
+		0.1f, 0.5f, 0.0f,
+		0.1f, -0.5f, 0.0f,
+		0.5f, 0.0f, 0.0f
 	};
+	/*
 	unsigned int indices[] = {
 		0, 1, 3,   // first triangle
 		1, 2, 3    // second triangle
 	};  
+	*/
 
 	// Initialize VAO
 	unsigned int VAO, VBO, EBO;
@@ -110,9 +117,11 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	/// Generate EBO
+	/* 
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	*/
 
 	// Linking
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -133,7 +142,9 @@ int main() {
 		// Draw
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 3, 3);
+		// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		// Check calls events and swap buffers
